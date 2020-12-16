f522041e98183f203a9224d463c27808ac76a077
Katiuska Alicea <katiuskaalicea@gmail.com>
2020-12-16 07:37:39 -0800

    created edit state in the item component but functionality is incomplete

--- src/Item.js
+++ src/Item.js
@@ -1,12 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Item = (props) => {
   console.log("props in Item component: ", props);
 
+  const [editing, setEditing] = useState(false);
+
+  const handleEdit = () => {
+    setEditing(!editing);
+  };
+
   return (
     <div>
-      {props.item.text}
-      <button onClick={() => props.handleDelete(props.item.id)}>Delete</button>
+      {editing ? (
+        <div>
+          {" "}
+          <input /> <button onClick={handleEdit}>Save</button>{" "}
+        </div>
+      ) : (
+        <div>
+          {props.item.text}
+          <button onClick={handleEdit}>Edit</button>
+          <button onClick={() => props.handleDelete(props.item.id)}>
+            Delete
+          </button>{" "}
+        </div>
+      )}
     </div>
   );
 };
